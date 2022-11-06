070f985fbc1c63e330fc1198392403c6e7cbd2bb
better-712 <89117847+better-712@users.noreply.github.com>
2022-11-06 17:39:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,27 +13,7 @@
 using std::cout, std::endl;
 
 int main(const int argc, const char **argv) {
-    if (argc == 2) {
-        SPL::SPL_Driver driver;
-
-        if (std::strncmp(argv[1], "-o", 2) == 0) {
-            driver.parse(std::cin);
-        } else if (std::strncmp(argv[1], "-h", 2) == 0) {
-            cout << "use -o for pipe to std::cin" << endl;
-            cout << "just give a filename to count from a file" << endl;
-            cout << "use -h to get this menu" << endl;
-            return EXIT_SUCCESS;
-        } else {
-            driver.parse(argv[1]);
-        }
-
-        if (driver.error_reported()) {
-            driver.print_errors();
-        } else {
-            SPL::print_ast(driver.get_root());
-        }
-    } else {
-        cout << "Only one parameter permitted! Use -h to see usage." << endl;
-        return EXIT_FAILURE;
-    }
+    SPL::SPL_Driver driver;
+    driver.parse(argv[1]);
+    
 }
