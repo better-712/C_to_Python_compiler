073a1dc8c212eb4ef960276bee19fd6691ce3ae5
better-712 <89117847+better-712@users.noreply.github.com>
2022-11-07 00:51:09 +0800

    Update ast.cpp

--- ast.cpp
+++ ast.cpp
@@ -6,25 +6,6 @@
 
 namespace SPL {
     
-    Node::Node(string type) : type{type} {}
-    
-    Node::Node(string type, Scan_Info *info) : type{type} {
-        line_no = info->line_no;
-        std::string lexeme = info->lexeme;
-        switch (this->leaf_type) {
-            case token::INT:
-                value = (unsigned int)std::stoul(lexeme, nullptr, 0);
-                break;
-            case token::FLOAT:
-                value = std::stof(lexeme, nullptr);
-                break;
-            case token::CHAR:
-                value = trim(lexeme, "'").c_str()[0];
-                break;
-            default:
-                break;
-        }
-    }
     void print_ast(Node *node, int level) {
         std::cout << node->type<<std::endl;
 //         if (node->is_empty()) {
