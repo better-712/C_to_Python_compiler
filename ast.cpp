f9d6f451ba366b0788ef4b2a589046fbb6ac73e4
better-712 <89117847+better-712@users.noreply.github.com>
2022-11-07 09:09:17 +0800

    Update ast.cpp

--- ast.cpp
+++ ast.cpp
@@ -16,8 +16,9 @@ namespace SPL {
         outfile.open("out.txt",std::ios::app);
         
         if(node->value.compare("INT")==0||node->value.compare("FLOAT")==0||node->value.compare("CHAR")==0||node->value.compare("TYPE")==0||node->value.compare("ID")==0)
+            outfile << std::string(2 * level, ' ')<< node->type <<": "<<node->value<< std::endl;
+        else 
             outfile << std::string(2 * level, ' ')<< node->type <<" ("<<std::to_string(node->line_no)<<")"<< std::endl;
-        else  outfile << std::string(2 * level, ' ')<< node->type <<": "<<node->value<< std::endl;
 
         for (auto &child: node->children) {
             print_ast(child, level + 1);
