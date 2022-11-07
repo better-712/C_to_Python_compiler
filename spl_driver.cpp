3ba89d57bb29f3c9f59b93b55688402b02d164d9
better-712 <89117847+better-712@users.noreply.github.com>
2022-11-07 10:41:06 +0800

    Update spl_driver.cpp

--- spl_driver.cpp
+++ spl_driver.cpp
@@ -48,4 +48,31 @@ namespace SPL {
     void SPL_Driver::add_syntax_error(std::string lexeme, Scan_Info *info) {
         syntax_errors->push_back(new Syntax_Error(lexeme, info->line_no));
     }
+    
+    
+    std::vector<Error *> *SPL_Driver::get_errors() {
+//        std::cout << "lexical errors: " << scanner->get_errors()->size() << std::endl;
+//        std::cout << "syntax errors: " << syntax_errors->size() << std::endl;
+        if (scanner->lexical_errors->empty()) {
+            return syntax_errors;
+        } else if (syntax_errors->empty()) {
+            return scanner->get_errors();
+        }
+
+        auto *all_errors = new std::vector<Error *>(scanner->get_errors()->begin(),
+                                                    scanner->get_errors()->end());
+
+        all_errors->insert(all_errors->end(), syntax_errors->begin(), syntax_errors->end());
+
+        std::sort(all_errors->begin(), all_errors->end(),
+                  [](Error *a, Error *b) { return a->info->line_no < b->info->line_no; });
+
+        return all_errors;
+    }
+    
+    void SPL_Driver::print_errors() {
+        for (const auto &error: *get_errors()) {
+            std::cout << error->to_string() << std::endl;
+        }
+    }
 }
