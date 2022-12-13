9753967ed6644cfae420474a05b9444f45200397
better-712 <89117847+better-712@users.noreply.github.com>
2022-12-13 17:42:41 +0800

    Update type.cpp

--- type.cpp
+++ type.cpp
@@ -18,8 +18,8 @@ namespace SPL{
     class Struct_Type : public Type {
     public:
         std::unordered_map<std::string, Type *> members;
-        Struct_Type(std::string name, std::vector<std::pair<std::string, Type *>> member_vector){
-          this->name=name;
+        Struct_Type(std::vector<std::pair<std::string, Type *>> member_vector){
+          this->name="Struct";
           for (auto &p: member_vector) 
             members.insert(p);
         }
