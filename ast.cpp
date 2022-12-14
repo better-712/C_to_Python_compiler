e1554ca3c16735508a54493a92ca05c2c1e8d758
better-712 <89117847+better-712@users.noreply.github.com>
2022-12-14 23:39:14 +0800

    Update ast.cpp

--- ast.cpp
+++ ast.cpp
@@ -112,34 +112,34 @@ namespace SPL {
     
     Type* get_type(Node* node){
         Type *specifier;
-        if(node->type.compare("StructSpecifier") == 0){
-            
-            std::vector<std::pair<std::string, Type *>> member_vector;
-            
-            Node* deflist=node->children[3];
-            std::vector<Node *>* def_list=list_to_element(deflist);
-            for(auto iter=def_list->begin();iter!=def_list->end();iter++){
-                Type *specifier=get_type((*iter)->children[0]->children[0]);
-                Node* declist=(*iter)->children[1];
-                std::vector<Node *>* dec_list=list_to_element(declist);
-                
-                for(auto iter=dec_list->begin();iter!=dec_list->end();iter++){
-                    std::string var=(*iter)->children.front()->children.front()->value;
-                    
-                    std::cout <<"name : "<<specifier->name<<std::endl;
-                    member_vector.push_back(make_pair(var,specifier));
-                    
-                }
-            }
-            
-            specifier=new Struct_Type(member_vector);
-            
-                
-        }
         if(node->type.compare("TYPE") == 0){
             specifier=new Primitive_Type(node->value);
-            
-        }   
+        } 
+        if(node->type.compare("StructSpecifier") == 0){
+            std::cout <<"size : "<<node->children.size()<<std::endl;
+            if(node->children.size()==2){
+                
+            }
+            else{
+                Node* deflist=node->children[3];
+                std::vector<std::pair<std::string, Type *>> member_vector;
+                std::vector<Node *>* def_list=list_to_element(deflist);
+                for(auto iter=def_list->begin();iter!=def_list->end();iter++){
+                    Type *specifier=get_type((*iter)->children[0]->children[0]);
+                    Node* declist=(*iter)->children[1];
+                    std::vector<Node *>* dec_list=list_to_element(declist);
+                
+                    for(auto iter=dec_list->begin();iter!=dec_list->end();iter++){
+                        std::string var=(*iter)->children.front()->children.front()->value;
+                        member_vector.push_back(make_pair(var,specifier));
+                    }
+                }
+                specifier=new Struct_Type(member_vector);
+                
+            }           
+                
+        }
+          
         return specifier;
     }
 //     std::pair<Type *, std::vector<VarDec_Node *> *> Local_Resolver::get_info(Def_Node *node) {
@@ -154,7 +154,12 @@ namespace SPL {
 
 //         return make_pair(specifier_type, decs);
 //     }
-
+    
+    void visit(Node *node){
+        for (auto &child: node->children) {
+            visit_node(child);
+        }
+    }
     
     void visit_node(Node *node) {
         if (node->type.compare("empty") == 0) {
@@ -164,12 +169,6 @@ namespace SPL {
             std::vector<Node*> children=node->children;
             Primitive_Type *specifier=new Primitive_Type(children[0]->children[0]->value);
             
-          
-//             std::cout <<"Specifier: "<<children[0]->type<<std::endl;
-            
-//             std::cout<<var[1]<<std::endl;
-            
-//            std::vector<std::string> var=get_Def(children[1]);
             std::vector<Node*> *decs =list_to_element(children[1]);
             auto *var = new std::vector<std::string>{};
             for(auto iter=decs->begin();iter!=decs->end();iter++)
@@ -184,7 +183,7 @@ namespace SPL {
             std::vector<Node*> children=node->children;
 //             std::string specifier=children[0]->type;
             Primitive_Type *specifier=new Primitive_Type(children[0]->children[0]->value);
-            if(node->children[1]->type.compare("ExtDecList") == 0){
+            if(children[1]->type.compare("ExtDecList") == 0){
                 
                 std::vector<Node*>  *vars =list_to_element(children[1]);
                 auto *var = new std::vector<std::string>{};
@@ -196,6 +195,7 @@ namespace SPL {
                     Variable_Entry *var=new Variable_Entry(*iter,specifier,children[0]->line_no);
                     insert(var);
                 }
+                return;
                 
             }
             if(children[1]->type.compare("FunDec") == 0){
@@ -214,21 +214,16 @@ namespace SPL {
                         Variable_Entry *var=new Variable_Entry(name,specifier,spec->line_no);
                         insert(var);
                     }
-//                         var->push_back((*iter)->children.back()->children.front()->value);
-                
-//                     for(auto iter=var->begin();iter!=var->end();iter++){
-//                         Variable_Entry *var=new Variable_Entry(*iter,specifier,children[0]->line_no);
-//                         insert(var);
-//                     }
-                    
+                          
                 }
-                
+                visit(children[1]);
             }
             if(children[0]->children[0]->type.compare("StructSpecifier") == 0){
                 Type* s=get_type(children[0]->children[0]);
                 std::string name=children[0]->children[0]->children[1]->value;
                 auto *symbol = new Struct_Def_Entry(name,dynamic_cast<Struct_Type*>(s));
                 insert(symbol);
+                return;
             }
             
         }
