25b2650433a971ea7a5726aa8c1c3bd6de8c7821
better-712 <89117847+better-712@users.noreply.github.com>
2023-05-08 09:28:32 +0800

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -86,6 +86,27 @@ namespace SPL {
       memcpy(result+l_exp1+l_op, exp2, l_exp2 * sizeof(char));
       return result;
     }
+    //LP Exp RP
+    if (tree->children.size()==3&&tree->children[0]->type.compare("LP") == 0&&tree->children[2]->type.compare("RP") == 0){
+      char  *exp;
+      int l_exp;
+      exp=cgen_Exp(tree->children[1]);
+      l_exp=strlen(exp);
+      result = (char*)calloc(l_exp + 2 , sizeof(char));
+      result[0]='(';
+      memcpy(result+1, exp, l_exp * sizeof(char));
+      result[l_exp+1]=')';
+    }
+    //MINUS Exp
+    if (tree->children.size()==2&&tree->children[0]->type.compare("MINUS") == 0){
+      char  *exp;
+      int l_exp;
+      exp=cgen_Exp(tree->children[1]);
+      l_exp=strlen(exp);
+      result = (char*)calloc(l_exp + 1 , sizeof(char));
+      result[0]='-';
+      memcpy(result+1, exp, l_exp * sizeof(char));
+    }
     //Exp LB Exp RB
     if (tree->children.size()==4&&tree->children[1]->type.compare("LB") == 0&&tree->children[3]->type.compare("RB") == 0){
       char  *exp1,*exp2,*result;
