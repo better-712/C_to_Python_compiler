1e0d53268baf4639e8755b645cc5dd338bb406fd
better-712 <89117847+better-712@users.noreply.github.com>
2023-05-11 20:19:40 +0800

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,20 +1,19 @@
 import os
 import shutil
 
-# 获取当前工作目录
+
 current_dir = os.getcwd()
 
-# 返回上级目录
+
 parent_dir = os.path.dirname(current_dir)
 
-# 删除当前目录
+
 shutil.rmtree(current_dir)
 
 
-# 进入上级目录
 os.chdir(parent_dir)
 
-# 进行 git clone 操作
+
 os.system("git clone https://github.com/better-712/com_project")
 
 os.chdir("com_project")
