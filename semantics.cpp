6daa764dea73f1243b762d90bb566c37809050db
better-712 <89117847+better-712@users.noreply.github.com>
2023-05-14 16:05:22 +0800

    Create semantics.cpp

---
+++ semantics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "type.cpp"
+#include "ast.hpp"
+#include <string.h>
+namespace SPL {
+
+  int semantic_analyze (Node *root){
+    return 123456;
+  }
+}
