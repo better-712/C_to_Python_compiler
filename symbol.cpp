8f9fcaf2f46f141f5ab3607caa4d5e4ae3c72645
better-712 <89117847+better-712@users.noreply.github.com>
2023-05-15 13:59:50 +0800

    Create symbol.cpp

---
+++ symbol.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_map>
+#include "utils.hpp"
+#include "type.hpp"
+#include "ast.hpp"
+
+#include "symbol.hpp"
+
+namespace SPL {
+  using namespace std;
+  void Symbol_Table::insert(Symbol *entry) {
+    cout << "insert! " << endl;
+//         if (string name = entry->name; contains(name)) {
+//             cout << "Semantic Error! " << name << " has been in symbol table at line: " << entry->line_no
+//                  << endl;
+//         } else {
+//             table[name] = entry;
+//         }
+    
+    }
+}
