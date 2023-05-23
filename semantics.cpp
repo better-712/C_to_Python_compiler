c63f7b876da76f3ef4adcc2235737a1d5b2a4571
better-712 <89117847+better-712@users.noreply.github.com>
2023-05-23 19:00:58 +0800

    Update semantics.cpp

--- semantics.cpp
+++ semantics.cpp
@@ -205,6 +205,7 @@ namespace SPL {
     //Exp DOT ID |struct fun
     if (tree->children.size()==3&&tree->children[1]->type.compare("DOT") == 0){
       Symbol_Type exp=analyze_Exp(tree->children[0]);
+      if(exp.type==-1)break;
       
       Symbol_Type ret;
       for (const auto& element : exp.parm_type) {
